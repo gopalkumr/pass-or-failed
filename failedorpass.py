f7d64d65ace657470dfa97873b56b14903b5e217
Gopal kumar <91552766+gopalkumr@users.noreply.github.com>
2021-10-25 15:38:57 +0530

    Update failedorpass.py

--- failedorpass.py
+++ failedorpass.py
@@ -9,7 +9,7 @@ avg_class = (no_of_persentclass - no_of_absentclass) / no_of_persentclass
 
 if (avg_grade >= 0.3):
     if(avg_class >= 0.8):
-        print('your grade is: ',avg_grade, n% 'your total class is: ',avg_class, 'You are pass , congratulations! ')
+        print('your grade is: ',avg_grade,  'your total class is: ',avg_class, 'You are pass , congratulations! ')
     else:
             print('your avrage grade is: ',round(avg_grade,3) ,'and your avarge class is: ',round(avg_class,3), 'you are failed ')
 else:
